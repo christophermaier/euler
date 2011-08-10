b3d06463d018f4c8f6fc4dafbce81eb4b597900c
Christopher Maier <christopher.maier@gmail.com>
2011-08-09 23:11:42 -0400

    Alternative solutions to Problem 3

--- src/euler/problem3.clj
+++ src/euler/problem3.clj
@@ -4,7 +4,8 @@ http://projecteuler.net/index.php?section=problems&id=3
 
 The prime factors of 13195 are 5, 7, 13 and 29.
 
-What is the largest prime factor of the number 600851475143?")
+What is the largest prime factor of the number 600851475143?"
+  (:use (clojure.contrib (math :only [sqrt]))))
 
 (defn primes
   "A lazy sequence of prime numbers"
@@ -30,8 +31,55 @@ What is the largest prime factor of the number 600851475143?")
   (if-let [smallest-prime-factor (first (filter #(= 0 (mod n %))
                                              (primes-less-than n)))]
     (lazy-cat [smallest-prime-factor]
-              (prime-factors-of (/ n smallest-prime-factor)))))
+              (prime-factors (/ n smallest-prime-factor)))))
 
 (defn largest-prime-factor
   [n]
   (last (prime-factors n)))
+
+;; ## Another Approach
+;; This is the more conventional approach.  It doesn't use lazy sequences,
+;; or computations of prime numbers at all, and so is much faster.
+
+(defn largest-prime-factor* [n]
+  (loop [num n factor 2]
+    (if (= 1 num)
+      factor
+      (if (= (mod num factor) 0)
+        (recur (/ num factor) factor)
+        (recur num (inc factor))))))
+
+;; ## Completely Factor Out The Twos
+;; If you completely factor out all the twos first, you can go ahead and
+;; skip testing all the even numbers.  This is faster still.
+
+(defn largest-prime-factor** [n]
+  (let [without-twos (loop [num n]
+                       (if (= (mod num 2) 0)
+                         (recur (/ num 2))
+                         num))]
+    (loop [num without-twos factor 3]
+      (if (= 1 num)
+        factor
+        (if (= (mod num factor) 0)
+          (recur (/ num factor) factor)
+          (recur num (+ factor 2)))))))
+
+
+;; ## Square Root Optimization
+;; A number can only have one prime factor greater than its square root.
+
+(defn largest-prime-factor***
+  [n]
+  (let [without-twos (loop [num n]
+                       (if (= (mod num 2) 0)
+                         (recur (/ num 2))
+                         num))]
+    (loop [num without-twos factor 3]
+      (if (>= factor (sqrt num))
+        (if (= 1 num)
+          factor
+          num)
+        (if (= (mod num factor) 0)
+          (recur (/ num factor) factor)
+          (recur num (+ factor 2)))))))
--- test/euler/test/problem3.clj
+++ test/euler/test/problem3.clj
@@ -2,14 +2,28 @@
   (:use euler.problem3 :reload)
   (:use clojure.test))
 
-(deftest sample-problem
-  (let [sample-number 13195]
+(let [sample-number 13195
+      sample-max 29
+
+      real-number 600851475143
+      real-max 6857]
+
+  (deftest sample-problem
     (is (= (prime-factors sample-number)
            (seq [5 7 13 29])))
     (is (= (largest-prime-factor sample-number)
-           29))))
+           sample-max)))
 
-(deftest real-problem
-  (let [real-number 600851475143]
+  (deftest real-problem
     (is (= (largest-prime-factor real-number)
-           6857))))
+           real-max)))
+
+  (deftest alternatives
+    (are [algorithm] (= (algorithm sample-number) sample-max)
+         largest-prime-factor*
+         largest-prime-factor**
+         largest-prime-factor***)
+    (are [algorithm] (= (algorithm real-number) real-max)
+         largest-prime-factor*
+         largest-prime-factor**
+         largest-prime-factor***)))
